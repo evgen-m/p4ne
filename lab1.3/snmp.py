8f36c5a226be1d6381d6533d8b3fa3e52c419006
evgen-m <xenon2295@gmail.com>
2021-03-17 19:03:25 +0300

    first commit

---
+++ lab1.3/snmp.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+
+from pysnmp.hlapi import *
+
+def print_snmp(g):
+    for snmp_result in g:
+        errorIndication, errorStatus, errorIndex, varBinds = snmp_result
+        if errorIndication:
+            print(errorIndication)
+            raise Snmp_exception
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+            raise Snmp_exception
+        else:
+            for varBind in varBinds:
+                print(varBind)
+                print(' = '.join([x.prettyPrint() for x in varBind]))
+
+try:
+    g = getCmd(SnmpEngine(),
+               CommunityData('public', mpModel=0),
+               UdpTransportTarget(('10.31.70.107', 161)),
+               ContextData(),
+               ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
+    print_snmp(g)
+
+    n = nextCmd(SnmpEngine(),
+               CommunityData('public', mpModel=0),
+               UdpTransportTarget(('10.31.70.107', 161)),
+               ContextData(),
+               ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')),
+               lexicographicMode=False)
+    print_snmp(n)
+
+except:
+    print('Error - exception')
